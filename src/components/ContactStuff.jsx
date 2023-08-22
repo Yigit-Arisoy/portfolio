f0b10557f1c54dbbf1b38c61c3191d9d7ef82f15
yiğit arısoy <arisoy.yigit@gmail.com>
2023-08-22 22:22:10 +0300

    added link for github page

--- src/components/ContactStuff.jsx
+++ src/components/ContactStuff.jsx
@@ -16,13 +16,22 @@ function ContactStuff() {
             <div className="text-white font-bold text-3xl sm:text-5xl">
               Behind the simulation
             </div>
-            <div className="text-neutral mt-8 text-sm sm:text-base mx-4 sm:mx-0">
+            <div className="text-neutral mt-8 mb-4 text-sm sm:text-base mx-4 sm:mx-0">
               You take the <span className="text-white">blue pill</span> - you
               keep scrolling, you leave this site and believe whatever you want
               to believe. You take the{" "}
               <span className="text-white">red pill</span> - you stay in
               wonderland, and I show you how deep the rabbit hole goes.
             </div>
+            <a
+              href="https://github.com/Yigit-Arisoy"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <div className="px-6 py-3 bg-neutral-focus rounded-sm text-bgMain text-lg font-semibold">
+                Show me
+              </div>
+            </a>
           </div>
         </div>
       </div>
